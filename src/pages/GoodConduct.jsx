c97776344e62a32c2967f96d8e14808dc91bf27c
Rumbustious <99288580+Rumbustious@users.noreply.github.com>
2024-04-16 21:56:19 +0200

    basic GoodConduct page style

--- src/pages/GoodConduct.jsx
+++ src/pages/GoodConduct.jsx
@@ -1,42 +1,49 @@
 import React from "react";
 import Header from "../Components/Header.jsx";
-// import Header from "../BaseComponents/Header.jsx";
-// import Footer2 from "../BaseComponents/Footer2.jsx";
-// import Subscription from "../BaseComponents/Subscription.jsx";
+
 function GoodConduct() {
     return (
-        <>
+        <div>
             <Header />
-            <section>
-                <h1>
-                    <u>شهادة حسن سير وسلوك</u>
-                </h1>
-                <h2> :تشهد كلية الهندسة بأن</h2>
-                <h3>
-                    <u>&nbsp;&nbsp;&nbsp;&nbsp;احمد محمد&nbsp;&nbsp;&nbsp;</u>
-                </h3>
+            <div
+                dir="rtl"
+                className="p-10  m-10 border-black border-2 rounded-md"
+            >
+                <div>
+                    <h1 className=" text-5xl font-bold text-center underline">
+                        شهادة حسن سير وسلوك
+                    </h1>
+                    <h2 className="text-2xl font-semibold">
+                        {" "}
+                        تشهد كلية الهندسة بأن:
+                    </h2>
+                    <h3 className="text-center font-bold text-4xl">
+                        {"أحمد محمد"}
+                    </h3>
 
-                <p>
-                    قد حصل على درجة البكالوريوس في
-                    الهندسة&nbsp;&nbsp;الكهربية&nbsp;&nbsp;&nbsp; شعبة
-                    &nbsp;&nbsp;اتصالات&nbsp;&nbsp;&nbsp;
-                </p>
-                <p>
-                    <p>
-                        حيث التحق
-                        بالكلية&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2015/2016&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;حتى&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2015/2016&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <p className="font-semibold text-center">
+                        قد حصل على درجة البكالوريوس في {"الهندسة الكهربية"}
+                        {"شعبة اتصالات"}
+                        <br />
+                        حيث التحق بالكلية
+                        <span className="font-bold m-3">{"2010/2011"}</span>
+                        حتى
+                        <span className="font-bold m-3">{"2015/2016"}</span>
+                        <br />
+                        ولم يوقع عليه اى جزاء قانوني او تأديبي
+                        <br />و قد أعطي له هذا بناء على طلبه وذلك لتقديمها الى
+                        من يهمه الأمر
+                        <br />
+                        دون ادنى مسؤلية على الكلية
                     </p>
-                </p>
-                <p>ولم يوقع عليه اى جزاء قانوني او تأديبي</p>
-                <p>
-                    و قدأعطي له هذه بناء على طلبه وذلك لتقديمها الى من يهمه
-                    الأمر
-                </p>
-                <p>دون ادنى مسؤلية على الكلية</p>
-            </section>
-            {/* <Subscription />
-            <Footer2 /> */}
-        </>
+                </div>
+            </div>
+            <div className="w-screen flex justify-center">
+                <button className="content-center font-bold px-5 py-2 bg-purple-400 text-white text-lg border rounded-sm hover:bg-blue-400 shadow-md">
+                    print
+                </button>
+            </div>
+        </div>
     );
 }
 export default GoodConduct;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            aspectRatio: {
+                paper: "2245 / 1587 ",
+            },
+        },
     },
     plugins: [],
 };
